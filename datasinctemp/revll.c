e0f8f675e2721d9cf826d436810a9c46fff9c9c8
sonuishaq67 <sonuishaq67@gmail.com>
2020-07-26 11:00:01 +0530

    Sunday 26 July 2020 11:00:01 AM IST

--- datasinctemp/revll.c
+++ datasinctemp/revll.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define null NULL
-
+#include <stdlib.h>
 struct node
 {
     int data;
@@ -8,19 +8,49 @@ struct node
 };
 typedef struct node node;
 node *head = null;
-node *temp = null;
-node *curr = null;
-node *head2 = null;
-
 void insert(int data)
 {
     node *tmp;
-    tmp=malloc(sizeof(node));
-    tmp->data=data;
-    tmp->next=head;
-    head=temp;
+    tmp = malloc(sizeof(node));
+    tmp->data = data;
+    tmp->next = head;
+    head = tmp;
 }
-
+int display()
+{
+    node *ptr = head;
+    int length = 0;
+    while (ptr)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+        length++;
+    }
+    printf("\n");
+    return length;
+}
+void reverse()
+{
+    node *prev = NULL;
+    node *curr = head;
+    node *next = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head=prev;
+}
 int main()
 {
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40);
+    display(head);
+    reverse(head);
+    display(head);
+    return 0;
 }
