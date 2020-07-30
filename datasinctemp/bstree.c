4c8bca6e5e00bcc05c3a615cec019fe1d4107eb6
sonuishaq67 <sonuishaq67@gmail.com>
2020-07-30 12:00:01 +0530

    Thursday 30 July 2020 12:00:01 PM IST

--- datasinctemp/bstree.c
+++ datasinctemp/bstree.c
@@ -39,12 +39,10 @@ int main()
     struct node *root = NULL;
     root = insert(root, 50);
     insert(root, 30);
-    insert(root, 90);
     insert(root, 20);
     insert(root, 40);
     insert(root, 70);
     insert(root, 60);
-    insert(root, 80);
     inorder(root);
     return 0;
 }
