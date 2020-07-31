71dbd42c2629d6dc3bbbb5831092dadeb6c8f45b
sonuishaq67 <sonuishaq67@gmail.com>
2020-07-31 17:00:01 +0530

    Friday 31 July 2020 05:00:01 PM IST

--- datasinctemp/priorityq.c
+++ datasinctemp/priorityq.c
@@ -44,6 +44,28 @@ void check(int data)
         }
     pri_que[i] = data;
 }
-void delete (int da)
+void delete (int data)
 {
+        int i;
+    if ((front==-1) && (rear==-1))
+    {
+        printf("\nQueue is empty no elements to delete");
+        return;
+    }
+    for (i = 0; i <= rear; i++)
+    {
+        if (data == pri_que[i])
+        {
+            for (; i < rear; i++)
+            {
+                pri_que[i] = pri_que[i + 1];
+            }
+        pri_que[i] = -99;
+        rear--;
+        if (rear == -1) 
+            front = -1;
+        return;
+        }
+    }
+    printf("\n%d not found in queue to delete", data);
 }
