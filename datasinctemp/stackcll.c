7a32691213153ebefc54db7682d46e786dcf56b4
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-16 20:00:01 +0530

    Sunday 16 August 2020 08:00:01 PM IST

---
+++ datasinctemp/stackcll.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+struct Node *top;
+
+void push(int data)
+{
+    struct Node *temp;
+    temp = malloc(sizeof(struct Node));
+    if (!temp)
+    {
+        printf("\n Overflow");
+        exit(1);
+    }
+    temp->data = data;
+    temp->link = top;
+    top = temp;
+}
+int isEmpty()
+{
+    return top == NULL;
+}
+int peek()
+{
+    if (!isEmpty())
+        return top->data;
+    else
+        exit(1);
+}
+void pop()
+{
+    struct Node *temp;
+    if (top == NULL)
+    {
+        printf("\n Underflow\n");
+        exit(1);
+    }
+    else
+    {
+        temp = top;
+        top = top->link;
+        temp->link = NULL;
+        free(temp);
+    }
+}
+
+void display()
+{
+    struct Node *temp;
+    if (top == NULL)
+    {
+        printf("\nStack Underflow");
+        exit(1);
+    }
+    else
+    {
+        temp = top;
+        printf("The stack is: ");
+        while (temp != NULL)
+        {
+            printf("%d ", temp->data);
+            temp = temp->link;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    push(11);
+    push(22);
+    push(33);
+    push(44);
+    display();
+    printf("The top element is %d\n", peek());
+    pop();
+    pop();
+    display();
+    printf("The top element is %d\n", peek());
+    return 0;
+}
