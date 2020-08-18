19f5afa7fe1326ce6d34710b92d16fbdc843acfc
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-18 10:00:01 +0530

    Tuesday 18 August 2020 10:00:01 AM IST

---
+++ datasinctemp/pol_ll.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int coeff;
+    int pow;
+    struct Node *next;
+};
+
+void insert(int coeff, int data)
+{
+}
+
+int main()
+{
+    int n=0;
+    while (n != -1)
+    {
+        printf("Enter two numbers: coeff and power");
+        int a,b;
+        scanf("%d %d",&a,&b);
+    }
+}
