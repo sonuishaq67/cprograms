c64d3e8580a3e56fc6f02e24a265a0e1b05d39f2
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-18 16:00:01 +0530

    Tuesday 18 August 2020 04:00:01 PM IST

--- datasinctemp/pol_ll.c
+++ datasinctemp/pol_ll.c
@@ -21,24 +21,41 @@ void insert(int coeff, int pow)
         struct Node *temp = head;
         while (temp)
         {
-            if (pow > temp->pow)
+            // if (pow > temp->pow)
+            // {
+            //     link->next = temp->next;
+            //     temp = temp->next; temp->next = link;
+            //     break;
+            // }
+            // else
+            //     temp = temp->next;
+            if (temp->next == NULL)
             {
-                link->next = temp;
+                temp->next = link;
+                break;
             }
             else
                 temp = temp->next;
         }
     }
 }
-
+void print()
+{
+    struct Node *temp = head;
+    while (temp->next)
+    {
+        printf("%dx^%d+",temp->coeff,temp->pow);
+        temp = temp->next;
+    }
+    printf("%dx^%d=0",temp->coeff,temp->pow);
+}
 int main()
 {
-    int n = 0;
-    while (n != -1)
-    {
-        printf("Enter two numbers: coeff and power");
-        int a, b;
-        scanf("%d %d", &a, &b);
-        insert(a, b);
-    }
+    insert(10, 1);
+    insert(20, 2);
+    insert(30, 3);
+    insert(40, 4);
+
+    print();
+    return 0;
 }
