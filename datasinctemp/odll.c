c45ecb9a4d658d1417a6a808c14387786cb73e9d
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-19 19:00:01 +0530

    Wednesday 19 August 2020 07:00:01 PM IST

---
+++ datasinctemp/odll.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define null NULL
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+Node *head = null;
+void insert(int data)
+{
+    Node *link = malloc(sizeof(Node));
+    link->data = data;
+    if (head == null)
+        head = link;
+    else
+    {
+        Node *temp = head;
+        while (temp->next && data < temp->data)
+        {
+            temp = temp->next;
+        }
+        link->prev = temp->prev;
+        link->next = temp->next;
+        temp = link;
+    }
+}
+
+void display()
+{
+    Node *temp = head;
+    while (temp)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    insert(10);
+    insert(20);
+    insert(30);
+    display();
+    return 0;
+}
